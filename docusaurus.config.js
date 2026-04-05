aaa94655b27ebb1a43985ff33907493ea2081b79
Johannes Lichtenwallner <johannes@lichtenwallner.at>
2026-04-05 09:57:34 +0200

    prod url to lichtenwallner.at

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jolicht.github.io',
+  url: 'https://lichtenwallner.at',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
